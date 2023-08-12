71a0dd4e8aff139cb6fa3c5cec3367426795dcb2
Andre Andre Andre <andredupdm@gmail.com>
2023-08-12 08:07:49 -0300

    infra: remove aws credentials

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  region     = "us-east-2"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = "us-east-2"
 }
 
 resource "aws_security_group" "sender-microservice-sg" {
