41b9fc198b24d012fb86797f6cb7bcbe707f1313
Andre Andre Andre <andredupdm@gmail.com>
2023-08-12 08:02:57 -0300

    infra: start terraform provider action

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region     = "us-east-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_security_group" "sender-microservice-sg" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "sender-microservice-instance" {
+  ami           = "ami-02f3416038bdb17fb"
+  instance_type = "t2.micro"
+  count         = 1
+  tags = {
+    name = "sender-microservice"
+    type = "main"
+  }
+
+  security_groups = ["${aws_security_group.sender-microservice-sg.name}"]
+}
